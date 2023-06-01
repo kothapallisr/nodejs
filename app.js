ff27200193a7c1ec624d27d1e5f99489c106603e
Sridhar Kothapalli <sridr8@gmail.com>
2023-06-01 18:53:44 +0530

    readline module

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
+const readline = require("readline");
 console.log("SaiSarath");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question("Please enter your name: ", (name) => {
+  console.log("You entered: " +name);
+  rl.close(0);
+})
+
+rl.on("close", () => {
+  console.log("Interface closed");
+  process.exit(0);
+})
